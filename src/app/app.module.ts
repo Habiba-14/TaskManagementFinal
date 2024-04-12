48307a5d614d3b8fe8d197cebd11156ef3fcad95
Habiba Ali <habeba.naser1412@gmail.com>
2024-04-12 07:14:49 +0200

    AssignmentCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskmanagementComponent } from './taskmanagement/taskmanagement.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskmanagementComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
